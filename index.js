4c001aafea82cf8457f56d3a586df26d9d16b6ec
ChangHyunKim <hot9998@naver.com>
2022-02-14 23:17:01 +0900

    Initialize project

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+  rules: {
+    'no-use-before-define': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'prefer-regex-literals': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+  },
+};
