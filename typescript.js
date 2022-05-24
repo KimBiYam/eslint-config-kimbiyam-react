28dc31f02c75fb25d96a9b4cf978db5f60dc3e61
ChangHyunKim <hot9998@naver.com>
2022-05-25 06:13:18 +0900

    Relocate no-shadow rule

--- typescript.js
+++ typescript.js
@@ -2,4 +2,5 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['plugin:@typescript-eslint/recommended'],
+  rules: { 'no-shadow': 'off' },
 };
